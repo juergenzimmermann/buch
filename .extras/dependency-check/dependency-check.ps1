6e47543d1cd5e05011259e0b8ea7606db2cec6db
Juergen Zimmermann <Juergen.Zimmermann@h-ka.de>
2025-01-17 20:02:12 +0100

    Updates: node-gyp, Powershell

--- .extras/dependency-check/dependency-check.ps1
+++ .extras/dependency-check/dependency-check.ps1
@@ -21,7 +21,7 @@
 
 Set-StrictMode -Version Latest
 
-$versionMinimum = [Version]'7.5.0'
+$versionMinimum = [Version]'7.6.0'
 $versionCurrent = $PSVersionTable.PSVersion
 if ($versionMinimum -gt $versionCurrent) {
   throw "PowerShell $versionMinimum statt $versionCurrent erforderlich"
