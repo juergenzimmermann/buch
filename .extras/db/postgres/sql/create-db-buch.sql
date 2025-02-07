04ad8b923824e1204f060e94c008634da32e0b19
Juergen Zimmermann <Juergen.Zimmermann@h-ka.de>
2025-02-07 09:39:52 +0100

    Update Nest; PostgreSQL: TLS mit netshoot ueberpruefen

--- .extras/db/postgres/sql/create-db-buch.sql
+++ .extras/db/postgres/sql/create-db-buch.sql
@@ -50,30 +50,21 @@
 --     docker compose exec db bash
 --        psql --dbname=postgres --username=postgres --file=/sql/create-db-buch.sql
 --        psql --dbname=buch --username=buch --file=/sql/create-schema-buch.sql
---        psql --dbname=postgres --username=postgres --file=/sql/create-db-sonar.sql
---        psql --dbname=sonar --username=sonar --file=/sql/create-schema-sonar.sql
 --        exit
---      docker compose down
+--     ggf. docker compose down
 
--- TLS fuer den PostgreSQL-Server mit OpenSSL in einer PowerShell ueberpruefen:
--- openssl s_client -connect localhost:5432 -starttls postgres
---
---  Erlaeuterung:
---      "openssl s_client": baut eine Verbindung ohne Verschlüsselung auf (hier: Rechner localhost mit Port 5432)
---      "starttls postgres": Kommando STARTTLS wird zum Server gesendet für ein Upgrade
---                           zu einer sicheren bzw. verschlüsselten Verbindung,
---                           um danach ein "TLS Handshake" mit dem Protokoll "postgres" durchzuführen
---
--- Ausgabe:
--- * "Can't use SSL_get_servername": Server gehört zu mehreren Domains bei derselben IP-Adresse
--- * selbst-signiertes Zertifikat
--- * "Certificate chain" mit Distinguished Name: S(ubject), CN (Common Name), OU (Organizationl Unit), O(rganization),
---                                               L(ocation), ST(ate), C(ountry)
--- * "No client certificate CA names sent": der OpenSSL-Client hat keine "Certificate Authority" (CA) Namen gesendet
--- * Algorithmus (signing digest): SHA256
--- * Signatur-Typ: RSA-PSS
--- * "TLSv1.3, Cipher is TLS_AES_256_GCM_SHA384"
--- * Schluessellaenge 2048 Bit
+-- TLS fuer den PostgreSQL-Server mit OpenSSL ueberpruefen:
+--     cd .extras\compose\debug
+--     docker compose up
+-- In einer weiteren Powershell
+--     cd .extras\compose\debug
+--     docker compose exec netshoot bash
+--         openssl s_client -tls1_3 -trace postgres:5432
+--            Ausgabe u.a.:
+--            * selbst-signiertes Zertifikat
+--            * Subject, CN (Common Name), OU (Organizationl Unit), O(rganization), L(ocation), ST(ate), C(ountry)
+--         exit
+--     docker compose down
 
 -- https://www.postgresql.org/docs/current/sql-createrole.html
 CREATE ROLE buch LOGIN PASSWORD 'p';
